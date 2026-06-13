aadba45f8f9cd8f78cc0bfd27aa172e9b894f127
Danil Shilov <94906421+JupiterHD@users.noreply.github.com>
2026-06-14 00:15:31 +0800

    Файлы исполнения и процесса

---
+++ process.py
@@ -0,0 +1,126 @@
+import random
+import math
+
+def create_implicants(n, vec):
+    
+    ones = {i for i, ch in enumerate(vec) if ch == '1'}
+
+    all_cubes = set()
+
+    for idx in ones:
+        bits = format(idx, f'0{n}b')
+        cube = tuple(bits)
+        all_cubes.add(cube)
+
+    return all_cubes
+
+def energy(c, ones, S1, S2, n):
+    p1 = 0
+    for el in c:
+        for ch in el:
+            if ch != '-':
+                p1 += 1
+
+    check = set()        
+    for el in c:
+        k = ['0']
+        for i in range(n):
+            # print(el[i])
+            if el[i] == '1':
+                for j in range(len(k)):
+                    k[j] = str(int(k[j]) + pow(2, n-i-1))
+            if el[i] == '-':
+                l = len(k)
+                for j in range(l):
+                    k.append(str(int(k[j]) + pow(2, n-i-1)))
+        for kk in k:
+            check.add(kk)
+
+    check1 = check & ones
+    check2 = check | ones
+    p2 = len(check2) - len(check1)
+
+    return p1*S1 + p2*S2
+
+def to_string(cubes):
+    arr = []
+    for el in cubes:
+        arr.append(list(el))
+    arr = sorted(arr)
+    ans = ""
+    for el in arr:
+        for i in range(len(el)):
+            if el[i] == "-":
+                continue
+            if el[i] == '0':
+                ans += "~"
+            ans += chr(ord('a') + i)
+        ans += " | "
+    else:
+        ans = ans[:-2] + "\n"
+    return ans
+
+def simulate_annealing(cubes, ones, n, S1, S2, T0, a, N):
+    
+
+    def new_cubes():
+        c = list(cubes)
+        t = random.randint(0, len(c))
+        k = random.randint(0, n-1)
+        r = random.randint(0, 2)
+        p = ['0', '1', '-']
+        if t == len(c):
+            c.append(tuple(['-']*n))
+        u = list(c[t])
+        u[k] = p[r]
+        c[t] = tuple(u)
+        c = set(c)
+        c.discard(tuple(['-', '-', '-']))
+        return c
+
+    lines = []
+
+    while T0 > 0:
+        mini = 1000*1000*1000
+        best = set()
+        for i in range(N):
+            c = new_cubes()
+            e = energy(c, ones, S1, S2, n)
+            if mini > e:
+                mini = e
+                best = c
+        delta = mini - energy(cubes, ones, S1, S2, n)
+        if delta <= 0:
+            cubes = best
+        elif random.random() < math.exp(-delta/T0):
+            cubes = best
+        lines.append(to_string(cubes))
+        T0 -= a
+
+    return cubes, lines
+        
+        
+def main():
+    n = int(input())
+    vec = input()
+    S1 = float(input())
+    S2 = float(input())
+    T0 = float(input())
+    a = float(input())
+    N = int(input())
+
+    ones = {str(i) for i, ch in enumerate(vec) if ch == '1'}
+
+    cubes = create_implicants(n, vec)
+    lines = []
+
+    result, lines = simulate_annealing(cubes, ones, n, S1, S2, T0, a, N)
+
+    with open("lines.txt", "w", encoding="utf-8") as file:
+        file.writelines(lines)
+
+    print(to_string(result))
+
+
+if __name__ == "__main__":
+    main()
